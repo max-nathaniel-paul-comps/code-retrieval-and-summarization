a25886dc4332cdd461143287f1d45a8394c385ce
shta2 <Joesayshi19>
2020-02-27 10:32:30 -0600

    more data

--- data2/preprocessing.py
+++ data2/preprocessing.py
@@ -1,9 +1,9 @@
 import csv
 
 
-file = open('data.csv', encoding='UTF8')
+file = open('data2.csv', encoding='UTF8')
 reader = csv.reader(file)
-newfile = open('processeed_data.csv', 'w')
+newfile = open('processeed_data2.csv', 'w', encoding='UTF8')
 writer = csv.writer(newfile)
 rows = 0
 for row in reader:
@@ -11,13 +11,21 @@ for row in reader:
         writer.writerow(row)
         rows += 1
         continue
-    if rows>20:
-        break
-    new_row = []
-    new_row.append(row[0])
+    # if rows>20:
+    #     break
+    title = row[0]
+    if title[-1] == '?':
+        title = title[:-1]
+    for opener in ['How do I', 'How do you', 'How can I', 'How to', 'Can I']:
+        if title.startswith(opener):
+            title = title[len(opener):]
     answer = row[1]
     code = answer[answer.find('<code>')+6:answer.find('</code>')]
-    if len(code) > 30:
-        new_row.append(code)
-        writer.writerow(new_row)
+    if len(code) > 30 and len(code) < 400:
+        writer.writerow([title,code])
         rows +=1
+
+        if rows % 10000 == 0:
+            print(rows)
+
+print(rows)
--- data2/scratch.sql
+++ data2/scratch.sql
@@ -3,4 +3,4 @@ select question.Title, answer.Body
   from posts as question, posts as answer
   where question.AcceptedAnswerID = answer.Id and question.Tags like '%<c#>%'
         and answer.Body like '%<code>%</code>%' and  answer.Body not like '%</code>%<code>%'
-        and LEN(answer.Body) > 30 and LEN(answer.Body) < 500
+        and LEN(answer.Body) > 100 and LEN(answer.Body) < 400
