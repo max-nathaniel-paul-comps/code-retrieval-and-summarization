a2dcc019f0252f7169266a14ec21aced2cd71445
Nathaniel Warner <nathanielwarner77@gmail.com>
2020-03-02 00:11:44 -0600

    Put back test set examples

--- src/train_model.py
+++ src/train_model.py
@@ -1,5 +1,5 @@
-import tensorflow_datasets as tfds
 import matplotlib.pyplot as plt
+import random
 from bvae import *
 
 
@@ -33,7 +33,7 @@ def main():
         train_summaries, train_codes, val_summaries, val_codes, test_summaries, test_codes
     )
 
-    latent_dim = 128
+    latent_dim = 256
 
     model = BimodalVariationalAutoEncoder(language_dim, language_tokenizer.vocab_size,
                                           source_code_dim, code_tokenizer.vocab_size,
@@ -44,7 +44,7 @@ def main():
     reduce_on_plateau = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=0)
     early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=20)
 
-    history = model.fit((train_summaries, train_codes), None, batch_size=64, epochs=30,
+    history = model.fit((train_summaries, train_codes), None, batch_size=64, epochs=1,
                         validation_data=((val_summaries, val_codes), None),
                         callbacks=[reduce_on_plateau, early_stopping])
 
@@ -68,6 +68,23 @@ def main():
 
     test_loss = model.evaluate((test_summaries, test_codes), None, batch_size=64, verbose=False)
     print("Test loss: " + str(test_loss))
+
+    for _ in range(20):
+        print()
+        random.seed()
+        random_idx = random.randrange(test_summaries.shape[0])
+        rand_test_summary = test_summaries[random_idx]
+        print("(Test Set) Input Summary: ", language_tokenizer.decode(rand_test_summary))
+        rand_test_code = test_codes[random_idx]
+        print("(Test Set) Input Code: ", code_tokenizer.decode(rand_test_code))
+        rec_summary = model.language_decoder(model.language_encoder(np.array([rand_test_summary])).mean())[0].numpy()
+        print("(Test Set) Reconstructed Summary: ", language_tokenizer.decode(np.argmax(rec_summary, axis=-1)))
+        rec_code = model.source_code_decoder(model.source_code_encoder(np.array([rand_test_code])).mean())[0].numpy()
+        print("(Test Set) Reconstructed Source Code: ", code_tokenizer.decode(np.argmax(rec_code, axis=-1)))
+        rec_summary_hard = model.language_decoder(model.source_code_encoder(np.array([rand_test_code])).mean())[0].numpy()
+        print("(Test Set) Reconstructed Summary From Source Code: ", language_tokenizer.decode(np.argmax(rec_summary_hard, axis=-1)))
+        rec_code_hard = model.source_code_decoder(model.language_encoder(np.array([rand_test_summary])).mean())[0].numpy()
+        print("(Test Set) Reconstructed Source Code From Summary: ", code_tokenizer.decode(np.argmax(rec_code_hard, axis=-1)))
 
 
 if __name__ == "__main__":
