472f402318b680ba2d8cbe9668efaf3aa001970d
Nathaniel Warner <nathanielwarner77@gmail.com>
2020-03-10 12:14:32 -0500

    Use ret-ir pt when doing baseline comparison

--- src/evaluate_retrieval.py
+++ src/evaluate_retrieval.py
@@ -11,9 +11,12 @@ def reciprocal_rank(sorted_indices, golden_idx):
             return 1.0 / (i + 1.0)
 
 
-def evaluate_retrieval(summaries, codes, baseline='ret_ir', random_sample_size=50, num_samples=20):
+def evaluate_retrieval(summaries, codes, baseline='ret_ir', random_sample_size=50, num_samples=200):
     if baseline == 'ret_ir':
-        baseline_model = lambda summary, code_snippets: retir.retir(summary, code_snippets, random_sample_size)
+        def baseline_model(summary, candidate_summaries):
+            summary = retir.shuffleQuery(summary, 0.2)
+            # Calling the version for pretokenized because ret-ir performs better at character-level for some reason
+            return retir.retir_pt(summary, candidate_summaries, random_sample_size)
     else:
         raise Exception("Invalid baseline specified: %s" % baseline)
     assert len(summaries) == len(codes)
@@ -27,13 +30,13 @@ def evaluate_retrieval(summaries, codes, baseline='ret_ir', random_sample_size=5
         rand_summaries = summaries[rand_idx: rand_idx + random_sample_size]
         rand_codes = codes[rand_idx: rand_idx + random_sample_size]
         golden_idx = random.randrange(random_sample_size)
-        """baseline_sorted_indices = baseline_model(rand_summaries[golden_idx], rand_codes)
-        baseline_reciprocal_ranks.append(reciprocal_rank(baseline_sorted_indices, golden_idx))"""
+        baseline_sorted_indices = baseline_model(rand_summaries[golden_idx], rand_summaries)
+        baseline_reciprocal_ranks.append(reciprocal_rank(baseline_sorted_indices, golden_idx))
         bvae_sorted_indices = bvae_model.rank_options(rand_summaries[golden_idx], rand_codes)
         bvae_reciprocal_ranks.append(reciprocal_rank(bvae_sorted_indices, golden_idx))
 
-    """baseline_mean_reciprocal_rank = np.mean(baseline_reciprocal_ranks)
-    print("Baseline Mean Reciprocal Rank: %s" % baseline_mean_reciprocal_rank)"""
+    baseline_mean_reciprocal_rank = np.mean(baseline_reciprocal_ranks)
+    print("Baseline Mean Reciprocal Rank: %s" % baseline_mean_reciprocal_rank)
 
     bvae_mean_reciprocal_rank = np.mean(bvae_reciprocal_ranks)
     print("BVAE Mean Reciprocal Rank: %s" % bvae_mean_reciprocal_rank)
