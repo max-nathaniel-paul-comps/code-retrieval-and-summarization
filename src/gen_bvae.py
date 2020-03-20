b1d5fbe6f625fe06f70dc8874c4910f90bcfa914
Nathaniel Warner <nathanielwarner77@gmail.com>
2020-03-19 23:09:38 -0700

    Add code generator

---
+++ src/gen_bvae.py
@@ -0,0 +1,37 @@
+import sys
+from bvae import *
+from seqifier import Seqifier
+
+
+def main():
+    assert len(sys.argv) == 2, "Usage: python gen_bvae.py path/to/model/dir/"
+    model_path = sys.argv[1]
+
+    print("Loading seqifiers, which are responsible for turning texts into sequences of integers...")
+    with open(model_path + "seqifiers_description.json") as seq_desc_json:
+        seqifiers_description = json.load(seq_desc_json)
+    language_seqifier = Seqifier(seqifiers_description['language_seq_type'],
+                                 model_path + seqifiers_description['language_seq_path'])
+    code_seqifier = Seqifier(seqifiers_description['source_code_seq_type'],
+                             model_path + seqifiers_description['source_code_seq_path'])
+
+    print("Loading model...")
+    model = BimodalVariationalAutoEncoder(model_path, language_seqifier, code_seqifier)
+    model.compile()
+    model.load_weights(model_path + "model_checkpoint.ckpt")
+
+    while True:
+        print()
+        query = input("Input Summary: ")
+        if query == "exit":
+            break
+        query_seq = language_seqifier.seqify_texts([query])
+        query_emb = model.language_embedding(query_seq)
+        latent = model.language_encoder(query_emb)
+        code_seq = model.source_code_decoder(latent.mean())
+        code = code_seqifier.de_seqify_texts(code_seq)[0]
+        print("Predicted Source Code: %s" % code)
+
+
+if __name__ == "__main__":
+    main()
