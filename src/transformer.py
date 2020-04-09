ccea87b5ab8799d5d57868a6d950e55d62b15821
Nathaniel Warner <nathanielwarner77@gmail.com>
2020-04-08 18:07:16 -0700

    Refactor transformer

--- src/transformer.py
+++ src/transformer.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import json
 import tqdm
+import sys
 
 import text_data_utils as tdu
 from tokenizer import Tokenizer
@@ -206,8 +207,10 @@ class DecoderLayer(tf.keras.layers.Layer):
 
 class Encoder(tf.keras.layers.Layer):
     def __init__(self, num_layers, d_model, num_heads, dff, input_vocab_size,
-                 maximum_position_encoding, rate=0.1):
+                 maximum_position_encoding, rate=0.1, universal=False):
         super(Encoder, self).__init__()
+
+        self.universal = universal
 
         self.d_model = d_model
         self.num_layers = num_layers
@@ -216,8 +219,11 @@ class Encoder(tf.keras.layers.Layer):
         self.pos_encoding = positional_encoding(maximum_position_encoding,
                                                 self.d_model)
 
-        self.enc_layers = [EncoderLayer(d_model, num_heads, dff, rate)
-                           for _ in range(num_layers)]
+        if self.universal:
+            self.enc_layer = EncoderLayer(d_model, num_heads, dff, rate)
+        else:
+            self.enc_layers = [EncoderLayer(d_model, num_heads, dff, rate)
+                               for _ in range(num_layers)]
 
         self.dropout = tf.keras.layers.Dropout(rate)
 
@@ -227,53 +233,29 @@ class Encoder(tf.keras.layers.Layer):
         # adding embedding and position encoding.
         x = self.embedding(x)  # (batch_size, input_seq_len, d_model)
         x *= tf.math.sqrt(tf.cast(self.d_model, tf.float32))
-        x += self.pos_encoding[:, :seq_len, :]
+
+        if not self.universal:
+            x += self.pos_encoding[:, :seq_len, :]
 
         x = self.dropout(x, training=training)
 
         for i in range(self.num_layers):
-            x = self.enc_layers[i](x, training, mask)
-
-        return x  # (batch_size, input_seq_len, d_model)
-
-
-class UTEncoder(tf.keras.layers.Layer):
-    def __init__(self, num_layers, d_model, num_heads, dff, input_vocab_size,
-                 maximum_position_encoding, rate=0.1):
-        super(UTEncoder, self).__init__()
-
-        self.d_model = d_model
-        self.num_layers = num_layers
-
-        self.embedding = tf.keras.layers.Embedding(input_vocab_size, d_model)
-        self.pos_encoding = positional_encoding(maximum_position_encoding,
-                                                self.d_model)
-
-        self.enc_layer = EncoderLayer(d_model, num_heads, dff, rate)
-
-        self.dropout = tf.keras.layers.Dropout(rate)
-
-    def call(self, x, training, mask):
-        seq_len = tf.shape(x)[1]
-
-        # adding embedding and position encoding.
-        x = self.embedding(x)  # (batch_size, input_seq_len, d_model)
-        x *= tf.math.sqrt(tf.cast(self.d_model, tf.float32))
-
-        x = self.dropout(x, training=training)
-
-        for t in range(self.num_layers):
-            x += self.pos_encoding[:, :seq_len, :]
-            x += self.pos_encoding[:, t, :]  # Timestep encoding
-            x = self.enc_layer(x, training, mask)
+            if self.universal:
+                x += self.pos_encoding[:, :seq_len, :]
+                x += self.pos_encoding[:, i, :]  # Timestep encoding
+                x = self.enc_layer(x, training, mask)
+            else:
+                x = self.enc_layers[i](x, training, mask)
 
         return x  # (batch_size, input_seq_len, d_model)
 
 
 class Decoder(tf.keras.layers.Layer):
     def __init__(self, num_layers, d_model, num_heads, dff, target_vocab_size,
-                 maximum_position_encoding, rate=0.1):
+                 maximum_position_encoding, rate=0.1, universal=False):
         super(Decoder, self).__init__()
+
+        self.universal = universal
 
         self.d_model = d_model
         self.num_layers = num_layers
@@ -292,53 +274,23 @@ class Decoder(tf.keras.layers.Layer):
 
         x = self.embedding(x)  # (batch_size, target_seq_len, d_model)
         x *= tf.math.sqrt(tf.cast(self.d_model, tf.float32))
-        x += self.pos_encoding[:, :seq_len, :]
+
+        if not self.universal:
+            x += self.pos_encoding[:, :seq_len, :]
 
         x = self.dropout(x, training=training)
 
         for i in range(self.num_layers):
-            x, block1, block2 = self.dec_layers[i](x, enc_output, training,
-                                                   look_ahead_mask, padding_mask)
+            if self.universal:
+                x += self.pos_encoding[:, :seq_len, :]
+                x += self.pos_encoding[:, t, :]  # Timestep encoding
+                x, block1, block2 = self.dec_layer(x, enc_output, training, look_ahead_mask, padding_mask)
+            else:
+                x, block1, block2 = self.dec_layers[i](x, enc_output, training,
+                                                       look_ahead_mask, padding_mask)
 
             attention_weights['decoder_layer{}_block1'.format(i + 1)] = block1
             attention_weights['decoder_layer{}_block2'.format(i + 1)] = block2
-
-        # x.shape == (batch_size, target_seq_len, d_model)
-        return x, attention_weights
-
-
-class UTDecoder(tf.keras.layers.Layer):
-    def __init__(self, num_layers, d_model, num_heads, dff, target_vocab_size,
-                 maximum_position_encoding, rate=0.1):
-        super(UTDecoder, self).__init__()
-
-        self.d_model = d_model
-        self.num_layers = num_layers
-
-        self.embedding = tf.keras.layers.Embedding(target_vocab_size, d_model)
-        self.pos_encoding = positional_encoding(maximum_position_encoding, d_model)
-
-        self.dec_layer = DecoderLayer(d_model, num_heads, dff, rate)
-
-        self.dropout = tf.keras.layers.Dropout(rate)
-
-    def call(self, x, enc_output, training,
-             look_ahead_mask, padding_mask):
-        seq_len = tf.shape(x)[1]
-        attention_weights = {}
-
-        x = self.embedding(x)  # (batch_size, target_seq_len, d_model)
-        x *= tf.math.sqrt(tf.cast(self.d_model, tf.float32))
-
-        x = self.dropout(x, training=training)
-
-        for t in range(self.num_layers):
-            x += self.pos_encoding[:, :seq_len, :]
-            x += self.pos_encoding[:, t, :]  # Timestep encoding
-            x, block1, block2 = self.dec_layer(x, enc_output, training, look_ahead_mask, padding_mask)
-
-            attention_weights['decoder_layer{}_block1'.format(t + 1)] = block1
-            attention_weights['decoder_layer{}_block2'.format(t + 1)] = block2
 
         # x.shape == (batch_size, target_seq_len, d_model)
         return x, attention_weights
@@ -356,16 +308,10 @@ class Transformer(tf.keras.Model):
         self.input_tokenizer = input_tokenizer
         self.output_tokenizer = output_tokenizer
 
-        if universal:
-            self.encoder = UTEncoder(num_layers, d_model, num_heads, dff,
-                                     input_vocab_size, pe_input, rate)
-            self.decoder = UTDecoder(num_layers, d_model, num_heads, dff,
-                                     target_vocab_size, pe_target, rate)
-        else:
-            self.encoder = Encoder(num_layers, d_model, num_heads, dff,
-                                   input_vocab_size, pe_input, rate)
-            self.decoder = Decoder(num_layers, d_model, num_heads, dff,
-                                   target_vocab_size, pe_target, rate)
+        self.encoder = Encoder(num_layers, d_model, num_heads, dff,
+                               input_vocab_size, pe_input, rate=rate, universal=universal)
+        self.decoder = Decoder(num_layers, d_model, num_heads, dff,
+                               target_vocab_size, pe_target, rate=rate, universal=universal)
 
         self.final_layer = tf.keras.layers.Dense(target_vocab_size)
 
@@ -633,41 +579,49 @@ def create_masks(inp, tar):
     return enc_padding_mask, combined_mask, dec_padding_mask
 
 
-def main(model_path="../models/transformer_11/", train=False):
+def main():
+
+    assert len(sys.argv) == 3
+    train = (sys.argv[1] == 'train')
+    model_path = sys.argv[2]
 
     print("Loading dataset...")
     train_summaries, train_codes = tdu.load_iyer_file("../data/iyer_csharp/train.txt")
     val_summaries, val_codes = tdu.load_iyer_file("../data/iyer_csharp/valid.txt")
 
-    print("Loading seqifiers, which are responsible for turning texts into sequences of integers...")
-    with open(model_path + "seqifiers_description.json") as seq_desc_json:
-        seqifiers_description = json.load(seq_desc_json)
+    print("Loading tokenizers...")
+    with open(model_path + "transformer_description.json") as transformer_desc_json:
+        transformer_description = json.load(transformer_desc_json)
 
-    language_seqifier = Tokenizer(seqifiers_description['language_seq_type'],
-                                  model_path + seqifiers_description['language_seq_path'],
-                                  training_texts=train_summaries,
-                                  target_vocab_size=seqifiers_description['language_target_vocab_size'])
-    code_seqifier = Tokenizer(seqifiers_description['source_code_seq_type'],
-                              model_path + seqifiers_description['source_code_seq_path'],
-                              training_texts=train_codes,
-                              target_vocab_size=seqifiers_description['source_code_target_vocab_size'])
+    language_tokenizer = Tokenizer(transformer_description['language_tokenizer_type'],
+                                   model_path + transformer_description['language_tokenizer_path'],
+                                   training_texts=train_summaries,
+                                   target_vocab_size=transformer_description['language_target_vocab_size'])
+    code_tokenizer = Tokenizer(transformer_description['code_tokenizer_type'],
+                               model_path + transformer_description['code_tokenizer_path'],
+                               training_texts=train_codes,
+                               target_vocab_size=transformer_description['code_target_vocab_size'])
 
     print("Loading transformer...")
 
-    num_layers = 4
-    d_model = 128
-    dff = 512
-    num_heads = 8
+    num_layers = transformer_description['num_layers']
+    d_model = transformer_description['d_model']
+    dff = transformer_description['dff']
+    num_heads = transformer_description['num_heads']
 
-    input_vocab_size = code_seqifier.vocab_size
-    target_vocab_size = language_seqifier.vocab_size
-    dropout_rate = 0.1
+    input_vocab_size = code_tokenizer.vocab_size
+    target_vocab_size = language_tokenizer.vocab_size
+    dropout_rate = transformer_description['dropout_rate']
+
+    universal = transformer_description['universal']
 
     transformer = Transformer(num_layers, d_model, num_heads, dff,
                               input_vocab_size, target_vocab_size,
                               input_vocab_size, target_vocab_size,
-                              model_path, code_seqifier, language_seqifier,
-                              rate=dropout_rate)
+                              model_path, code_tokenizer, language_tokenizer,
+                              max_input_len=transformer_description['c_dim'],
+                              max_output_len=transformer_description['l_dim'],
+                              rate=dropout_rate, universal=universal)
 
     if train:
         transformer.train(train_codes, train_summaries, val_codes, val_summaries, num_epochs=15)
