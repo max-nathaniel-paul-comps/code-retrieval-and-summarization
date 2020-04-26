d78a86e5313bda8bc45e1195fb1eddbf577f3b0f
Nathaniel Warner <nathanielwarner77@gmail.com>
2020-04-26 14:05:00 -0700

    Fix IR

--- baselines/IR/ir.py
+++ baselines/IR/ir.py
@@ -1,126 +1,47 @@
 import textdistance
-import random
 import sys
-from operator import itemgetter
 from nltk import download
-from nltk.translate.bleu_score import sentence_bleu
+from nltk.translate.bleu_score import corpus_bleu
 from nltk.translate.meteor_score import meteor_score
+import numpy as np
 sys.path.append('../../src')
 import text_data_utils as tdu
 
-def setup(numToTake):
-    summaries, codes = tdu.load_iyer_file("../../data/iyer_csharp/test.txt")
-    sumSnips, codeSnips = randSample(summaries, codes, numToTake)
-    return sumSnips, codeSnips
 
-def altSetup(numToTake):
-    dataset = tdu.load_iyer_dataset("../../data/iyer_csharp/dev.txt", "../../data/iyer_csharp/dev_alternate_summaries.txt")
-    summaries = [ex[0] for ex in dataset]
-    codes = [ex[1] for ex in dataset]
-    alt_summaries = [ex[2][0] for ex in dataset]
-    sumSnips, codeSnips, altSnips = randSample(summaries, codes, numToTake, True, alt_summaries)
-    return sumSnips, codeSnips, altSnips
-
-def randSample(summaries, codes, num, doingAlts = False, altSums = None):
-    used = []
-    sumRet = []
-    codRet = []
-    altRet = []
-    for x in range(num):
-        choice = random.randint(0, len(summaries)-1)
-        #put this in just to bother you nathaniel :)
-        while choice in used:
-            choice = random.randint(0, len(summaries)-1)
-        used.append(choice)
-        sumRet.append(summaries[choice])
-        codRet.append(codes[choice])
-        if doingAlts:
-            altRet.append(altSums[choice])
-    if doingAlts:
-        return sumRet, codRet, altRet
-    else:
-        return sumRet, codRet
-
-def shuffleQuery(q):
-    q = q[3:]
-    q = q[:-4]
-    returner = ""
-    for w in q.split(" "):
-        if random.randint(0, 9) > 2:
-            returner += w + " "
-    returner = "<s>" + returner + "</s>"
-    return returner
-
-def ir(snippet, sums, codes):
-    assert len(sums)==len(codes)
-    dists = []
-    for c in range(len(codes)):
-        dists.append([textdistance.levenshtein(snippet, codes[c]), c])
-    sorted(dists, key=itemgetter(0))
-    return sums[dists[0][1]]
-
-def unTokenize(tokenListList):
-    returner = []
-    for tokenList in tokenListList:
-        temp = ""
-        for t in tokenList:
-            temp += t + " "
-        returner.append(temp)
-    return returner
-
-def eval_ir(sums, codes, alts, codeSnippet, codeSnippetIndex, areTokenized):
-    correct = sums[codeSnippetIndex]
-    candidate = ir(alts[codeSnippetIndex], sums, codes)
-    bleu = -1.0
-    meteor = -1.0
-    if areTokenized:
-        bleu = sentence_bleu(sums, candidate)
-        meteor = round(meteor_score(unTokenize(sums), candidate), 6)
-    else:
-        print("BLEU NON-TOKENIZED NOT IMPLEMENTED")
-        meteor = round(meteor_score(sums, candidate), 6)
-    print("METEOR SCORE: ", meteor)
-    print("BLEU-4 SCORE: ", bleu)
-    return meteor, bleu
-
-def test_ir(sums, codes, num_tests, intensify, altSnips, test_num):
-    assert len(sums) == len(codes)
-    msum = 0
-    bsum = 0
-    for x in range(num_tests):
-        if intensify:
-            sums, codes = setup(test_num)
-        cs = random.choice(list(range(len(codes))))
-        tmeteor, tbleu = eval_ir(sums, codes, altSnips, codes[cs], cs, True)
-        msum += tmeteor
-        bsum += tbleu
-    msum = msum / num_tests
-    bsum = bsum / num_tests
-    print("Average METEOR: ", msum)
-    print("Average BLEU: ", bsum)
-
-def test_ir_iyer(num_tests, test_num, intensify, doAlts):
-    if not doAlts:
-        sumSnips, codeSnips = setup(test_num)
-        altSnips = None
-    else:
-        print("this")
-        sumSnips, codeSnips, altSnips = altSetup(test_num)
-    #sums, codes = tdu.trim_to_len(summaries, codess, trim_num, trim_num)
-    test_ir(sumSnips, codeSnips, num_tests, intensify, altSnips, test_num)
-
-if __name__=="__main__":
-    #download('wordnet')
-    print("How many tests would you like to run? (10)")
-    num_tests = int(input())
-    print("How many snippets in the test? (50)")
-    num_snips = int(input())
-    print("Intensify? y/n")
-    do = False
-    if input()=="y":
-        do = True
-    test_ir_iyer(num_tests, num_snips, do, True)
-        
-    
+def ir(code, summaries):
+    code = tdu.tokenize_text(code)
+    summaries_tok = tdu.tokenize_texts(summaries)
+    dists = np.array([textdistance.levenshtein(code, summaries_tok[i]) for i in range(len(summaries_tok))])
+    min_dist_idx = int(np.argmin(dists))
+    return summaries[min_dist_idx]
 
 
+def test_ir_iyer():
+    download('wordnet')
+    dataset = tdu.load_iyer_dataset("../../data/iyer_csharp/dev.txt",
+                                    alternate_summaries_filename="../../data/iyer_csharp/dev_alternate_summaries.txt")
+    all_non_alt_summaries = [ex[0] for ex in dataset]
+    refs = []
+    preds = []
+    meteors = []
+    for i in range(len(dataset)):
+        code = dataset[i][1]
+        prediction = ir(code, all_non_alt_summaries)
+        alt_summaries = dataset[i][2]
+        print("Code: %s" % code)
+        print("True summaries: %s" % alt_summaries)
+        print("Predicted summary: %s" % prediction)
+        meteor = meteor_score(alt_summaries, prediction)
+        print("Sentence METEOR score: %.4f" % meteor)
+        print()
+        refs.append(alt_summaries)
+        preds.append(prediction)
+        meteors.append(meteor)
+    meteor = np.mean(meteors)
+    bleu = corpus_bleu(refs, preds)
+    print("Average METEOR: ", meteor)
+    print("Corpus BLEU-4: ", bleu)
+
+
+if __name__ == "__main__":
+    test_ir_iyer()
