545bd5ce061b8a357fb4f00ef6f1d54e8692165f
shta2 <Joesayshi19>
2020-05-07 13:49:23 -0400

    created simple gui and optional alternative scheme for idf

---
+++ GUI/gui.py
@@ -0,0 +1,62 @@
+import sys
+# sys.path.append("..\\src")
+from tkinter import *
+# from bvae import *
+
+
+class GUI:
+    def __init__(self, master):
+        self.master = master
+
+        # self.label = Label(master, text="Code Retrieval and Summarization")
+        # self.label.pack()
+
+        self.input = Text(master)
+        self.input.pack()
+
+        self.summary_button_frame = Frame(root)
+        self.summary_button_frame.pack()
+
+        self.retrieval_button_frame = Frame(root)
+        self.retrieval_button_frame.pack()
+
+        self.summarize_bt = Button(self.summary_button_frame, text="Generate Summary", command=self.summarize)
+        self.summarize_bt.pack(side=LEFT)
+        self.baseline_summarize_bt = Button(self.summary_button_frame, text="Generate Summary with Baseline Model", command=self.baseline_summarize)
+        self.baseline_summarize_bt.pack(side=LEFT)
+
+        self.retrieve_bt = Button(self.retrieval_button_frame, text="Retrieve Code", command=self.retrieve)
+        self.retrieve_bt.pack(side=LEFT)
+        self.baseline_retrieve_bt = Button(self.retrieval_button_frame, text="Retrieve Code with Baseline Model", command=self.baseline_retrieve)
+        self.baseline_retrieve_bt.pack(side=LEFT)
+
+        self.close_button = Button(master, text="Close", command=master.quit)
+        self.close_button.pack()
+
+        self.output = Text(master)
+        self.output.pack()
+
+    def greet(self):
+        print("Greetings!")
+
+    def summarize(self):
+        self.output.delete(1.0, END)
+        # self.output.insert(END, "Error: Unable to produce a summary")
+        code = self.input.get(1.0, END)
+        self.output.insert(END, "The code does " + code)
+
+    def baseline_summarize(self):
+        self.output.delete(1.0, END)
+        self.output.insert(END, "Error: Unable to produce a summary with Baseline")
+
+    def retrieve(self):
+        self.output.delete(1.0, END)
+        self.output.insert(END, "def success():\n\tprint(success)")
+
+    def baseline_retrieve(self):
+        self.output.delete(1.0, END)
+        self.output.insert(END, "Error: Unable to retrieve code with Baseline")
+
+root = Tk()
+gui = GUI(root)
+root.mainloop()
